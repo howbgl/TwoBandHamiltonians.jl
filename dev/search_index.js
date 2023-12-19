bc5e52f7548bbc3c442a9de268dc7842d962e842
Documenter.jl <documenter@juliadocs.github.io>
2023-12-19 11:25:38 +0000

    build based on 7dec9ee

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TwoBandHamiltonians","category":"page"},{"location":"#TwoBandHamiltonians","page":"Home","title":"TwoBandHamiltonians","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TwoBandHamiltonians.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TwoBandHamiltonians]","category":"page"}]
+}
