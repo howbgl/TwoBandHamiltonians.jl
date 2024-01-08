fdc07593f01959cb66c0e2fd106cd22601b34849
Wolfgang Hogger <wolfgang.hogger@ur.de>
2024-01-08 13:53:10 +0100

    Added example to docstring

--- src/GappedDirac.jl
+++ src/GappedDirac.jl
@@ -11,6 +11,12 @@ The Hamiltonian reads
 ``` 
 such that ``\\vec{h}=[k_x,k_y,m]``.
 
+# Examples
+```jldoctest
+julia> h = GappedDirac(1.0)
+GappedDirac{Float64}(1.0)
+```
+
 # See also
 [`GeneralTwoBand`](@ref TwoBandHamiltonians.GeneralTwoBand)
 """
@@ -30,3 +36,13 @@ jac(h::GappedDirac,kx,ky) = SA[
     one(typeof(h.m)) zero(typeof(h.m)) zero(typeof(h.m))
     zero(typeof(h.m)) one(typeof(h.m)) zero(typeof(h.m))
     zero(typeof(h.m)) zero(typeof(h.m)) zero(typeof(h.m))]
+
+function printparamsSI(h::GappedDirac,us::UnitScaling;digits=3)
+
+    m   = energySI(h.m,us)
+    vF  = velocitySI(one(h.m),us)
+    
+    return """
+        m  = $(round(typeof(m),m,sigdigits=digits)) ($(h.m))
+        vF = $(round(typeof(vF),vF,sigdigits=digits)) ($(1.0))"""
+end
